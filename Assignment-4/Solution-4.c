e005e93a9e614b6bfc09d484dfa828c03cdf8138
cdpatra <109278272+cdpatra@users.noreply.github.com>
2022-07-26 23:03:27 +0530

    Add files via upload

---
+++ Assignment-4/Solution-4.c
@@ -0,0 +1,8 @@
+// 4. Write a program to print the first 10 odd natural numbers
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+        printf("%d ", 2 * i - 1);
+    return 0;
+}
---
+++ Assignment-4/Solution-7.c
@@ -0,0 +1,8 @@
+// 7. Write a program to print the first 10 even natural numbers in reverse order
+#include <stdio.h>
+int main()
+{
+    for (int i = 10; i > 0; i--)
+        printf("%d ", 2 * i);
+    return 0;
+}
